febda1791049187155c14cbe8ceff0df21ef5b31
Stephen Karukas <stephen.karukas@gmail.com>
2019-08-24 22:01:54 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Stephen Karukas on 8/24/19.
+//  Copyright © 2019 Stephen Karukas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
