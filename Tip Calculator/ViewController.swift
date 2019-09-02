3192f0f5421ae8947003650cf0a1631061069cbc
Stephen Karukas <stephen.karukas@gmail.com>
2019-09-01 21:15:22 -0400

    add locale capability

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var customTipField: UITextField!
+    @IBOutlet weak var customTipView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func updateBill(_ sender: Any) {
+        // get bill amount
+        let bill = Double(billField.text!) ?? 0
+        let customTip = Int(customTipField.text!) ?? 0
+        
+        // calc bill and total
+        let tipPercentages = [15, 18, 22, customTip]
+        let selectedIndex = tipControl.selectedSegmentIndex
+        customTipView.isHidden = selectedIndex != 3
+        let tipPercent = tipPercentages[selectedIndex]
+        let tip = bill * Double(tipPercent) / 100.0
+        let total = tip + bill
+        
+        // format currency
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.locale = Locale.current
+        print(Locale.current.identifier)
+        currencyFormatter.numberStyle = NumberFormatter.Style.currency
+        let totalString = currencyFormatter.string(for: total)
+        let tipString = currencyFormatter.string(for: tip)
+        
+        // update bill and total
+        tipLabel.text = tipString
+        totalLabel.text = totalString
+        customTipField.text = String(tipPercent)
+    }
 }
 
